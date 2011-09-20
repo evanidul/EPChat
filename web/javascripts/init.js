364fb715c3fcdbbef0a4463c0b2598fa04f82365
dvu <evanidul@gmail.com>
2011-09-19 17:16:46 -0700

    put ep in an iframe

--- web/javascripts/init.js
+++ web/javascripts/init.js
@@ -8,10 +8,11 @@ function initYUIPanels(){
 	YAHOO.ep.common.chat.manager = new YAHOO.widget.OverlayManager();
 	/** init yui panels **/
 	YAHOO.ep.common.chat.chatWindow = new YAHOO.widget.Panel("main", 
-		{xy:[200,200],visible:false,width:"500px",/*height:"190px",*/ zIndex:1006, 
+		{xy:[200,200],visible:false,width:"225px",height:"350px", zIndex:1006, 
 			effect:{effect:YAHOO.widget.ContainerEffect.FADE, duration: 0.3}, constraintoviewport:false});
 	YAHOO.ep.common.chat.chatWindow.render();
 	YAHOO.ep.common.chat.chatWindow.show();
 	YAHOO.ep.common.chat.manager.register([YAHOO.ep.common.chat.chatWindow]);
 }
+
 
